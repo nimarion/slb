bc7d895ee50633459163a1998d94c92ce56f9524
Niklas Marion <niklas.marion@gmx.de>
2023-10-30 19:29:21 +0100

    validate club with club in data from federation

---
+++ validate/wrong_club.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+year = 2023
+
+athletes = pd.read_csv('validate/' + str(year) + '/athletes.csv')
+data = pd.read_csv('data/' + str(year) + '.csv')
+
+data = data.merge(athletes, left_on=['firstname', 'lastname', 'birthyear'], right_on=['firstname', 'lastname', 'birthyear'])
+data = data[data['club_x'] != data['club_y']]
+data = data[~data['club_x'].str.contains('StG')]
+data.drop(columns=['athleteId', 'sex', 'country', 'result', 'ageGroup', 'date', 'location', 'teamResult'], inplace=True)
+data.rename(columns={'club_x': 'Verein in Bestenliste', 'club_y': 'Verein in Startrecht'}, inplace=True)
+
+cols = list(data.columns.values)
+cols.pop(cols.index('Verein in Bestenliste'))
+cols.pop(cols.index('Verein in Startrecht'))
+data = data[cols+['Verein in Bestenliste', 'Verein in Startrecht']]
+
+print(data)
