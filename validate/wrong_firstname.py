ed9b301f1f448fe51f5f2bf5e8755359a124bc5a
Niklas Marion <niklas.marion@gmx.de>
2023-11-01 21:39:47 +0100

    update data

--- validate/wrong_firstname.py
+++ validate/wrong_firstname.py
@@ -5,18 +5,17 @@ year = 2023
 athletes = pd.read_csv('validate/' + str(year) + '/athletes.csv')
 data = pd.read_csv('data/' + str(year) + '.csv')
 
-# Create a new column in both DataFrames with the first two letters of the 'firstname'
-data['firstname_2letters'] = data['firstname'].str[:2] + data['firstname'].str[-2:]
-athletes['firstname_2letters'] = athletes['firstname'].str[:2] + athletes['firstname'].str[-2:]
 
-# Merge DataFrames on 'firstname_2letters', 'lastname', and 'club'
+shortest = data[data['firstname'].str.len() > 2]['firstname'].str.len().min()
+
+data['firstname_2letters'] = data['firstname'].str[:shortest]
+athletes['firstname_2letters'] = athletes['firstname'].str[:shortest]
+
 merged_data = data.merge(athletes, on=['firstname_2letters', 'lastname', 'club', 'birthyear'])
 
 
-# Filter out rows where 'lastname' from both DataFrames is not equal
 filtered_data = merged_data[merged_data['firstname_x'] != merged_data['firstname_y']]
 
-# Drop the 'firstname_2letters' column if no longer needed
 filtered_data = filtered_data.drop('firstname_2letters', axis=1)
 filtered_data.drop(columns=['athleteId', 'sex', 'country', 'result', 'location', 'teamResult', 'place', 'birthyear'], inplace=True)
 filtered_data.rename(columns={'firstname_x': 'Vorname in Bestenliste', 'firstname_y': 'Vorname in Startrecht'}, inplace=True)
@@ -27,6 +26,10 @@ cols.pop(cols.index('Vorname in Bestenliste'))
 cols.pop(cols.index('Vorname in Startrecht'))
 filtered_data = filtered_data[cols+['lastname', 'Vorname in Bestenliste', 'Vorname in Startrecht']]
 
+#filtered_data = filtered_data.drop_duplicates(subset=['lastname', 'Vorname in Bestenliste', 'Vorname in Startrecht'], keep='first')
+filtered_data.rename(columns={'lastname': 'Nachname', 'club': 'Verein', 'date': 'Datum', 'ageGroup': 'Altersklasse', 'discipline': 'Disziplin'} , inplace=True)
 filtered_data.to_excel('tmp/wrong_firstname_' + str(year) + '.xlsx', index=False)
+
+
 
 print(filtered_data)
--- validate/wrong_lastname.py
+++ validate/wrong_lastname.py
@@ -5,20 +5,21 @@ year = 2023
 athletes = pd.read_csv('validate/' + str(year) + '/athletes.csv')
 data = pd.read_csv('data/' + str(year) + '.csv')
 
-# Create a new column in both DataFrames with the first two letters of the 'lastname'
-data['lastname_2letters'] = data['lastname'].str[:2] + data['lastname'].str[-2:]
-athletes['lastname_2letters'] = athletes['lastname'].str[:2] + athletes['lastname'].str[-2:]
+shortest = data['lastname'].str.len().min()
 
-# Merge DataFrames on 'lastname_2letters', 'lastname', and 'club'
+print(shortest)
+
+data['lastname_2letters'] = data['lastname'].str[:shortest]
+athletes['lastname_2letters'] = athletes['lastname'].str[:shortest]
+
 merged_data = data.merge(athletes, on=['lastname_2letters', 'firstname', 'club', 'birthyear'])
 
 
-# Filter out rows where 'lastname' from both DataFrames is not equal
 filtered_data = merged_data[merged_data['lastname_x'] != merged_data['lastname_y']]
 
-# Drop the 'lastname_2letters' column if no longer needed
 filtered_data = filtered_data.drop('lastname_2letters', axis=1)
-filtered_data.drop(columns=['athleteId', 'sex', 'country', 'result', 'location', 'teamResult', 'place'], inplace=True)
+filtered_data.drop(columns=['athleteId', 'sex', 'country', 'result', 'location', 'teamResult', 'place', 'birthyear'], inplace=True)
+
 filtered_data.rename(columns={'lastname_x': 'Nachname in Bestenliste', 'lastname_y': 'Nachname in Startrecht'}, inplace=True)
 
 cols = list(filtered_data.columns.values)
@@ -26,6 +27,9 @@ cols.pop(cols.index('firstname'))
 cols.pop(cols.index('Nachname in Bestenliste'))
 cols.pop(cols.index('Nachname in Startrecht'))
 filtered_data = filtered_data[cols+['firstname', 'Nachname in Bestenliste', 'Nachname in Startrecht']]
+
+#filtered_data = filtered_data.drop_duplicates(subset=['firstname', 'Nachname in Bestenliste', 'Nachname in Startrecht'], keep='first')
+
 
 filtered_data.to_excel('tmp/wrong_lastname_' + str(year) + '.xlsx', index=False)
 
